709b78d56b10f1e4f5616794b13da5b5463bcaf7
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2021-12-27 13:06:26 +0100

    Menu gitfix

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,148 @@
+import * as React from "react";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import PropTypes from "prop-types";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+import { useNavigate } from "react-router-dom";
+
+const drawerWidth = 240;
+
+function MainMenu(props) {
+  const navigate = useNavigate();
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawerList = [
+    {
+      text: "Pending for approval",
+      navigateTo: "pending-requests",
+    },
+    {
+      text: "Team",
+      navigateTo: "team",
+    },
+    {
+      text: "Questions",
+      navigateTo: "questions",
+    },
+    {
+      text: "Company Info",
+      navigateTo: "company-info",
+    },
+    {
+      text: "My Profile",
+      navigateTo: "my-profile",
+    },
+  ];
+
+  const drawer = (
+    <div>
+      <List>
+        {drawerList.map((item, index) => {
+          const { text, navigateTo } = item;
+          return (
+            <ListItem
+              button
+              key={text}
+              onClick={() => navigate(`/${navigateTo}`)}
+            >
+              <ListItemIcon>
+                <ArrowForwardIosIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <p style={{ fontSize: "1rem" }}>Menu</p>
+        </IconButton>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="menu"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+MainMenu.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+export default MainMenu;
