13623c7d3e399d0d1dec95427803e0ee1b89b9a9
filipstev <filipstevanovic176@gmail.com>
2022-01-05 11:45:21 +0100

    resen konflikt

--- src/Components/Questions/AddQuestion/AddQuestion.js
+++ src/Components/Questions/AddQuestion/AddQuestion.js
@@ -1,126 +1,110 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../../Header/Header';
 import {
-  TextField,
-  Button,
-  FormControl,
-  Select,
-  InputLabel,
-  MenuItem,
-  FormHelperText,
+    TextField,
+    Button,
+    FormControl,
+    Select,
+    InputLabel,
+    MenuItem,
+    FormHelperText,
 } from '@mui/material';
 import axiosInstance from '../../../helpers/axiosInstance';
 import { useNavigate } from 'react-router-dom';
 
 const AddQuestion = (props) => {
-  const [text, setText] = useState('');
-  const [type, setType] = useState('text');
-  const [questionsLength, setQuestionsLength] = useState(null);
-  const navigate = useNavigate();
+    const [text, setText] = useState('');
+    const [type, setType] = useState('text');
+    const [questionsLength, setQuestionsLength] = useState(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    axiosInstance
-      .get('/questions')
-      .then((data) => {
-        setQuestionsLength(data.data.data.length);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+    useEffect(() => {
+        axiosInstance
+            .get('/questions')
+            .then((data) => {
+                setQuestionsLength(data.data.data.length);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-  const submitQuestion = async () => {
-    if (text !== '' && type && questionsLength >= 0) {
-      await axiosInstance
-        .post('/questions', {
-          data: {
-            text: text,
-            type: type,
-            order: questionsLength,
-          },
-        })
-        .then((data) => {
-          console.log('success');
-          console.log(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    const submitQuestion = async () => {
+        if (text !== '' && type && questionsLength >= 0) {
+            await axiosInstance
+                .post('/questions', {
+                    data: {
+                        text: text,
+                        type: type,
+                        order: questionsLength,
+                    },
+                })
+                .then((data) => {
+                    console.log('success');
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            navigate('/questions');
+        }
+    };
 
-      navigate('/questions');
-    }
-  };
-
-<<<<<<< HEAD
-  return (
-    <>
-      <div
-        style={{
-          width: '50%',
-          margin: 'auto',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-        }}
-      >
-        <h2
-          style={{
-            marginBottom: '40px',
-            marginTop: '30px',
-            fontSize: '28px',
-            lineHeight: '32px',
-            letterSpacing: '0.04em',
-          }}
-        >
-          Add new Question
-        </h2>
-        <TextField
-          label="Question text"
-          variant="outlined"
-          fullWidth="true"
-          style={{ marginBottom: '40px' }}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <FormControl sx={{ minWidth: 300, marginBottom: '30px' }}>
-          {/* <FormHelperText>Question type</FormHelperText> */}
-          <InputLabel id="qtype-label">Question type</InputLabel>
-          <Select
-            labelId="qtype-label"
-            id="qtype"
-            value={type}
-            label="Question Type"
-            onChange={(e) => setType(e.target.value)}
-          >
-            <MenuItem value={'text'}>Text</MenuItem>
-            <MenuItem value={'long'}>Long text</MenuItem>
-            <MenuItem value={'image'}>Image</MenuItem>
-          </Select>
-        </FormControl>
-        <Button
-          style={{ alignSelf: 'flex-end' }}
-          variant="outlined"
-          onClick={submitQuestion}
-        >
-          Save
-        </Button>
-      </div>
-    </>
-  );
-=======
-const AddQuestion = () => {
     return (
         <>
-            <Header />
-            <div>
-                <div>Add new Question</div>
-                <div></div>
-                <div></div>
-                <div>Save</div>
+            <div
+                style={{
+                    width: '50%',
+                    margin: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                }}
+            >
+                <h2
+                    style={{
+                        marginBottom: '40px',
+                        marginTop: '30px',
+                        fontSize: '28px',
+                        lineHeight: '32px',
+                        letterSpacing: '0.04em',
+                    }}
+                >
+                    Add new Question
+                </h2>
+                <TextField
+                    label="Question text"
+                    variant="outlined"
+                    fullWidth="true"
+                    style={{ marginBottom: '40px' }}
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <FormControl sx={{ minWidth: 300, marginBottom: '30px' }}>
+                    {/* <FormHelperText>Question type</FormHelperText> */}
+                    <InputLabel id="qtype-label">Question type</InputLabel>
+                    <Select
+                        labelId="qtype-label"
+                        id="qtype"
+                        value={type}
+                        label="Question Type"
+                        onChange={(e) => setType(e.target.value)}
+                    >
+                        <MenuItem value={'text'}>Text</MenuItem>
+                        <MenuItem value={'long'}>Long text</MenuItem>
+                        <MenuItem value={'image'}>Image</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button
+                    style={{ alignSelf: 'flex-end' }}
+                    variant="outlined"
+                    onClick={submitQuestion}
+                >
+                    Save
+                </Button>
             </div>
         </>
     );
->>>>>>> teams-page
 };
 
 export default AddQuestion;
--- src/Components/Questions/SingleQuestion/SingleQuestion.js
+++ src/Components/Questions/SingleQuestion/SingleQuestion.js
@@ -3,63 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../../helpers/axiosInstance';
 import classes from './SingleQuestion.module.css';
 
-<<<<<<< HEAD
 const SingleQuestion = (props) => {
-  const navigate = useNavigate();
-  const onEdit = () => {
-    navigate('/edit-question', { state: { id: props.id } });
-  };
+    const navigate = useNavigate();
+    const onEdit = () => {
+        navigate('/edit-question', { state: { id: props.id } });
+    };
 
-  return (
-    <div
-      style={{
-        width: '100%',
-        border: '2px solid #797979',
-        display: 'flex',
-        justifyContent: 'space-between',
-        padding: '20px',
-        alignItems: 'center',
-        marginBottom: '18px',
-        boxSizing: 'border-box',
-      }}
-    >
-      <div className={classes.Left}>
-        <div className={classes.Arrows}>
-          {/* TODO:CUSTOM ICONS */}
-          <i
-            className="fas fa-angle-up"
-            onClick={() => {
-              if (!props.blocked) {
-                props.moveQuestion('up', props.id);
-              }
-            }}
-          ></i>
-          <i
-            className="fas fa-angle-down"
-            onClick={() => {
-              if (!props.blocked) {
-                props.moveQuestion('down', props.id);
-              }
-            }}
-          ></i>
-        </div>
-        <div className={classes.Text}>
-          <div
-            style={{
-              fontWeight: 'bold',
-              fontSize: '12px',
-              lineHeight: '14px',
-              letterSpacing: '0.04em',
-            }}
-          >
-            Question {props.order + 1} - {props.type}
-          </div>
-          <div
-=======
-const SingleQuestion = () => {
     return (
         <div
->>>>>>> teams-page
             style={{
                 width: '100%',
                 border: '2px solid #797979',
@@ -70,32 +21,26 @@ const SingleQuestion = () => {
                 marginBottom: '18px',
                 boxSizing: 'border-box',
             }}
-<<<<<<< HEAD
-          >
-            {props.title}
-          </div>
-        </div>
-      </div>
-      <div className={classes.Right}>
-        <div className={classes.ButtonEdit} onClick={onEdit}>
-          Edit
-        </div>
-        <div
-          className={classes.ButtonDelete}
-          onClick={() => props.deleteQuestion(props.id)}
-        >
-          Delete
-        </div>
-      </div>
-    </div>
-  );
-=======
         >
             <div className={classes.Left}>
                 <div className={classes.Arrows}>
                     {/* TODO:CUSTOM ICONS */}
-                    <i class="fas fa-angle-up"></i>
-                    <i class="fas fa-angle-down"></i>
+                    <i
+                        className="fas fa-angle-up"
+                        onClick={() => {
+                            if (!props.blocked) {
+                                props.moveQuestion('up', props.id);
+                            }
+                        }}
+                    ></i>
+                    <i
+                        className="fas fa-angle-down"
+                        onClick={() => {
+                            if (!props.blocked) {
+                                props.moveQuestion('down', props.id);
+                            }
+                        }}
+                    ></i>
                 </div>
                 <div className={classes.Text}>
                     <div
@@ -106,26 +51,37 @@ const SingleQuestion = () => {
                             letterSpacing: '0.04em',
                         }}
                     >
-                        Question 1 - Text
+                        Question {props.order + 1} - {props.type}
                     </div>
                     <div
                         style={{
-                            fontSize: '20px',
-                            lineHeight: '23px',
-                            marginTop: '4px',
+                            width: '100%',
+                            border: '2px solid #797979',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            padding: '20px',
+                            alignItems: 'center',
+                            marginBottom: '18px',
+                            boxSizing: 'border-box',
                         }}
                     >
-                        Do you have any pets?
+                        {props.title}
                     </div>
                 </div>
             </div>
             <div className={classes.Right}>
-                <div className={classes.ButtonEdit}>Edit</div>
-                <div className={classes.ButtonDelete}>Delete</div>
+                <div className={classes.ButtonEdit} onClick={onEdit}>
+                    Edit
+                </div>
+                <div
+                    className={classes.ButtonDelete}
+                    onClick={() => props.deleteQuestion(props.id)}
+                >
+                    Delete
+                </div>
             </div>
         </div>
     );
->>>>>>> teams-page
 };
 
 export default SingleQuestion;
