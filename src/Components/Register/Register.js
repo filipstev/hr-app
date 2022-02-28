99a7b3642aed5a1fbcb48f148ae940c53af707bf
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2022-02-28 13:28:07 +0100

    number of companies in Select Company

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -40,14 +40,16 @@ const Register = () => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     useEffect(() => {
-        axiosInstance.get(`/companies`).then(({ data }) => {
-            setCompanies(data.data);
-            if (slug) {
-                setCompany(slug);
-                return;
-            }
-            setCompany(data.data[0].attributes.slug);
-        });
+        axiosInstance
+            .get(`/companies?pagination[pageSize]=50`)
+            .then(({ data }) => {
+                setCompanies(data.data);
+                if (slug) {
+                    setCompany(slug);
+                    return;
+                }
+                setCompany(data.data[0].attributes.slug);
+            });
     }, [slug]);
 
     const {
